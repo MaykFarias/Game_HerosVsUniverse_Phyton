9a971d045f42b5e7c3a83b93097d9984d2b8bbbb
Mayk <maykfariasy@gmail.com>
2023-03-11 11:07:54 -0300

    Validaçãod e dados e design

--- HeroisVsViloes.py
+++ HeroisVsViloes.py
@@ -1,8 +1,8 @@
 import random
 import time
 
-listaDeHerois = ["Superman", "Batman", "Mulher Maravilha"]
-listaDeViloes = ["Coringa", "Lex Luthor", "Flash Reverso"]
+listaDeHerois = ["\033[31:1mSuperman\033[m", "\033[36:1mBatman\033[m", "\033[33:1mMulher Maravilha\033[m"]
+listaDeViloes = ["\033[35:1mCoringa\033[m", "\033[37:1mLex Luthor\033[m", "\033[32:1mDuende Verde\033[m"]
 listaDeAtaques = [10, 20, 40]
 
 vidaHeroi = 100
@@ -12,61 +12,94 @@ escolhaVilao = 0
 numeroGolpeHeroi = 0
 numeroGolpeVilao = 0
 
+## APRESENTAÇÃO DOS HEROIS
+print("")
 print(" -" * 15)
-print("\033[0:1m|        JOGO DOS HEROIS       |\033[m")
+print(f"\033[0:1m{f'JOGO DOS HEROIS':^30}\033[m")
 print(" -" * 15)
-print("\033[0:1m QUAL HEROI VOCÊ ESCOLHE ?\033[m")
-print("\033[0:36m [1] Superman \n [2] Batmam \n [3] Mulher Maravilha\033[m ")
+print("\033[0:1mQUAL HEROI VOCÊ ESCOLHE ?\033[m")
+for i, hero in enumerate(listaDeHerois):
+    print(f"[{i + 1}] {hero}")
+print("Digite o numero do heroi que voce deseja: ")
 print("-" * 30)
-escolhaUsuario = int(input("---> "))
+escolhaUsuario = 0
+while True:
+    escolhaUsuario = input("Escolha um ataque (1, 2 ou 3): ").strip()
+    try:
+        escolhaUsuario = int(escolhaUsuario)
+        if escolhaUsuario not in (1, 2, 3):
+            print("Escolha inválida. Tente novamente.")
+        else:
+            break
+    except ValueError:
+        print("Entrada inválida. Tente novamente.")
 escolhaHeroi = listaDeHerois[escolhaUsuario - 1]
 print(f"Você escolheu o {escolhaHeroi}")
 print("-" * 30)
-
 time.sleep(1)
+
+## ESCOLHA DO COMPUTADOR
 print("O COMPUTADOR VAI ESCOLHER O SEU OPONENTE !")
 escolhaPc = random.randint(1, 3)
 escolhaVilao = listaDeViloes[escolhaPc - 1]
 print(f"O oponente escolhido foi... {escolhaVilao}\n")
-
 time.sleep(2)
+
+## HOUNDS
 print("-" * 30)
-print("\033[0:1m|             HOUND            |\033[m")
+print(f"\033[30:0:1m{f'HOUND':^30}\033[m")
 print("-" * 30)
 print("\033[0:1mQuantos hounds deseja jogar ?\033[m")
 print("-" * 15)
-escolhaDosHounds = int(input("---> "))
+while True:
+    escolhaDosHounds = int(input("---> "))
+    try:
+        escolhaDeHounds = int(escolhaDosHounds)
+        break
+    except ValueError:
+        print("Entrada inválida. Tente novamente.")
 print("-" * 15)
 print(" ")
-
-time.sleep(2)
 print("= " * 15)
+
+## APRESENTAÇÃO
 print("\033[0:1mPREPARANDO CAMPO DE BATALHA...\n")
 time.sleep(2)
-print(f"{f'{escolhaHeroi} _X_ {escolhaVilao} ':^30}")
-print(f"{f'Vida 100         Vida  100  %':^5}")
-print(f"SERÁ UM PARTIDA DE {escolhaDosHounds} hounds.\033[m")
+print(f"{escolhaHeroi} _X_ {escolhaVilao}")
+print(f"{f'Vida 100%         Vida  100%':^5}")
+print(f"SERÁ UM PARTIDA DE {escolhaDosHounds} HOUNDS.\033[m")
 print("= " * 15)
 
+## LUTA
 time.sleep(2)
 while vidaHeroi > 0 and vidaVilao > 0:
-    while escolhaDosHounds > 0:
+    while escolhaDeHounds > 0:
+        print("")
+        print(f"\033[30:0:1m{f'HOUND ':>17}{escolhaDosHounds}\033[m")
         if vidaVilao > 0:
-            print(" ")
             print("__" * 15)
-            print("           OPÇÔES ")
+            print(f"{'OPÇÔES':^30}")
             print("=" * 30)
             print(
                 "[1] ATAQUE FRACO \n" +
                 "[2] ATAQUE FORTE\n" +
                 "[3] ATAQUE CRITICO")
-            print("=_" * 15)
-            print("= QUAL O SEU COMANDO ?           ")
-            escolhaDeAtaqueUsuario = int(input("-----> "))
-            vidaVilao -= listaDeAtaques[escolhaDeAtaqueUsuario-1]
-            print("=" * 20)
+            print("=" * 30)
+            escolhaDeAtaqueUsuario = 0
+            while True:
+                escolhaDeAtaqueUsuario = input("Escolha um ataque (1, 2 ou 3): ").strip()
+                try:
+                    escolhaDeAtaqueUsuario = int(escolhaDeAtaqueUsuario)
+                    if escolhaDeAtaqueUsuario not in (1, 2, 3):
+                        print("Escolha inválida. Tente novamente.")
+                    else:
+                        break
+                except ValueError:
+                    print("Entrada inválida. Tente novamente.")
+            vidaVilao -= listaDeAtaques[escolhaDeAtaqueUsuario - 1]
+            print("=" * 30)
             print(
-                f"\033[0:1m{escolhaHeroi} casou {listaDeAtaques[escolhaDeAtaqueUsuario-1]} de dano no {escolhaVilao}\033[m")
+                f"\033[0:1m{escolhaHeroi} casou {listaDeAtaques[escolhaDeAtaqueUsuario - 1]} de dano no {escolhaVilao}\033[m")
             time.sleep(2)
             if vidaVilao <= 0:
                 break
@@ -79,8 +112,8 @@ while vidaHeroi > 0 and vidaVilao > 0:
                 break
 
         time.sleep(2)
-        print(f"{escolhaHeroi} VIDA: {vidaHeroi}%")
-        print(f"{escolhaVilao} VIDA: {vidaVilao}%")
+        print(f"{escolhaHeroi} VIDA: \033[1m{vidaHeroi}%\033[m")
+        print(f"{escolhaVilao} VIDA: \033[1m{vidaVilao}%\033[1m")
 
         escolhaDosHounds -= 1
     print("")
@@ -94,5 +127,6 @@ while vidaHeroi > 0 and vidaVilao > 0:
     elif (vidaHeroi <= 0 or vidaVilao <= 0):
         print("EMPATE !!!")
         break
+
 time.sleep(2)
 print("Fim do Jogo !!!")
